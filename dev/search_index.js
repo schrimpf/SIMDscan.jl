450d1b47445429c674ab055b0b45bad812dc73b0
Documenter.jl <documenter@juliadocs.github.io>
2023-09-29 01:02:03 +0000

    build based on 60b2443

---
+++ dev/search_index.js
@@ -0,0 +1,3 @@
+var documenterSearchIndex = {"docs":
+[{"location":"","page":"Home","title":"Home","text":"CurrentModule = SIMDscan","category":"page"},{"location":"#SIMDscan","page":"Home","title":"SIMDscan","text":"","category":"section"},{"location":"","page":"Home","title":"Home","text":"Documentation for SIMDscan.","category":"page"},{"location":"","page":"Home","title":"Home","text":"","category":"page"},{"location":"","page":"Home","title":"Home","text":"Modules = [SIMDscan]","category":"page"}]
+}
