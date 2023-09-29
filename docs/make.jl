3051e86d63fc93908b1accca6ea77b2a7e328970
Paul Schrimpf <paul.schrimpf@ubc.ca>
2023-09-28 20:53:24 -0700

    Fixing documentation build

--- docs/make.jl
+++ docs/make.jl
@@ -1,25 +1,24 @@
-using Documenter
+using Documenter, SIMDscan
 
 DocMeta.setdocmeta!(SIMDscan, :DocTestSetup, :(using SIMDscan); recursive=true)
 
 makedocs(;
-    modules=[SIMDscan],
-    authors="Paul Schrimpf <paul.schrimpf@gmail.com> and contributors",
-    repo="https://github.com/schrimpf/SIMDscan.jl/blob/{commit}{path}#{line}",
-    sitename="SIMDscan.jl",
-    format=Documenter.HTML(;
-        prettyurls=get(ENV, "CI", "false") == "true",
-        canonical="https://schrimpf.github.io/SIMDscan.jl",
-        edit_link="main",
-        assets=String[],
-    ),
-    pages=[
-        "Home" => "index.md",
-        "Benchmarks" => "benchmarks.md"
-    ],
-)
+         modules=[SIMDscan],
+         authors="Paul Schrimpf <paul.schrimpf@ubc.ca> and contributors",
+         repo=Remotes.GitHub("schrimpf","SIMDscan.jl"),
+         sitename="SIMDscan.jl",
+         format=Documenter.HTML(;
+                                prettyurls=get(ENV, "CI", "false") == "true",
+                                edit_link="main",
+                                assets=String[],
+                                ),
+         pages=[
+           "Home" => "index.md",
+           "Benchmarks" => "benchmarks.md"
+         ]
+         )
 
 deploydocs(;
-    repo="github.com/schrimpf/SIMDscan.jl",
-    devbranch="main",
+           repo="github.com/schrimpf/SIMDscan.jl",
+           devbranch="main"
 )
